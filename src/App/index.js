c29323fe21f20477caab78c39623194a6aaa66db
Oleksandr Vykhor <sashavihor67@gmail.com>
2023-05-29 14:42:01 +0200

    added  result for hook

--- src/App/index.js
+++ src/App/index.js
@@ -1,33 +1,13 @@
-import React, { useState } from "react";
-import { currencies } from "./currencies";
 import Form from "./Form";
 import { Clock } from "./Clock";
 import { Wrapper } from "./styled";
 
 function App() {
-    const [result, setResult] = useState();
-
-    const calculateResult = (currency, amount) => {
-        const rate = currencies
-            .find(({ short }) => short === currency)
-            .rate;
-
-        setResult({
-            sourceAmount: +amount,
-            targetAmount: amount / rate,
-            currency,
-
-        });
-
-    }
-
+    
     return (
         <Wrapper>
             <Clock />
-            <Form
-                result={result}
-                calculateResult={calculateResult}
-            />
+            <Form/>
         </Wrapper>
     );
 
